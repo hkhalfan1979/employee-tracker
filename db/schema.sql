fec3d6713c08d5e8d670f62ed6b7978380d56afe
hasnainkhalfan <73541651+hasnainkhalfan@users.noreply.github.com>
2022-05-25 18:39:16 -0400

    setup and started db

---
+++ db/schema.sql
@@ -0,0 +1,9 @@
+DROP DATABASE IF EXISTS employee_DB;
+CREATE database employee_DB;
+
+USE employee_DB;
+
+CREATE TABLE department (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30) NOT NULL
+);

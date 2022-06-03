b547df4c2aade63cd72a09ce9da5179e9c016893
hasnainkhalfan <73541651+hasnainkhalfan@users.noreply.github.com>
2022-06-03 15:44:39 -0400

    sorted our mysql connection and inquirer

--- server.js
+++ server.js
@@ -1,120 +1,86 @@
-// List the dependencies here.
-const mysql = require('mysql');
+require('dotenv').config();
+//console.log(process.env);
+const mysql = require('mysql2');
 const inquirer = require('inquirer');
-const consoleTable = require('console.table');
-const util = require('util');
+const consoleTable = require('console.table')
 
-// Create the connection to MySQL WorkBench
-let connection = mysql.createConnection({
+// creates connection to sql database
+const connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
     user: 'root',
     password: '',
-    database: 'employee_DB'
-});
-
-connection.query = util.promisify(connection.query);
-
-// Begin the application after establishing the connection.
-connection.connect(function (err) {
-    if (err) throw err;
-    console.log("Connected!");
-    initialAction();
+    database: 'employees_db'
 })
 
-// Give the user a pleasant welcome message.
-console.table(
-    "\n------------ EMPLOYEE TRACKER ------------\n"
-)
+// connects to sql server and sql database
+connection.connect(function(err){
+    if (err) throw err;
+    console.log("Connected!");
+    options();
+})
 
-// Ask the user initial action question to figure out what they would like to do.
-const initialAction = async () => {
-    try {
-        let answer = await inquirer.prompt({
+// prompts user with list of options to choose from
+function options() {
+    inquirer
+        .prompt({
             name: 'action',
             type: 'list',
-            message: 'What would you like to do?',
+            message: 'Welcome to our employee database! What would you like to do?',
             choices: [
-                'View Employees',
-                'View Departments',
-                'View Roles',
-                'Add Employees',
-                'Add Departments',
-                'Add Roles',
-                'Update Employee Role',
-                'Exit'
-            ]
-        });
-        switch (answer.action) {
-            case 'View Employees':
-                employeeView();
-                break;
+                    'View all employees',
+                    'View all departments',
+                    'View all roles',
+                    'Add an employee',
+                    'Add a department',
+                    'Add a role',
+                    'Update employee role',
+                    'Delete an employee',
+                    'EXIT'
+                    ]
+            }).then(function (answer) {
+                switch (answer.action) {
+                    case 'View all employees':
+                        viewEmployees();
+                        break;
+                    case 'View all departments':
+                        viewDepartments();
+                        break;
+                    case 'View all roles':
+                        viewRoles();
+                        break;
+                    case 'Add an employee':
+                        addEmployee();
+                        break;
+                    case 'Add a department':
+                        addDepartment();
+                        break;
+                    case 'Add a role':
+                        addRole();
+                        break;
+                    case 'Update employee role':
+                        updateRole();
+                        break;
+                    case 'Delete an employee':
+                        deleteEmployee();
+                        break;
+                    case 'EXIT': 
+                        exitApp();
+                        break;
+                    default:
+                        break;
+                }
+        })
+};
 
-            case 'View Departments':
-                departmentView();
-                break;
-
-            case 'View Roles':
-                roleView();
-                break;
-
-            case 'Add Employees':
-                employeeAdd();
-                break
-
-            case 'Add Departments':
-                departmentAdd();
-                break
-
-            case 'Add Roles':
-                roleAdd();
-                break
-
-            case 'Update Employee Role':
-                employeeUpdate();
-                break
-
-            case 'Exit':
-                connection.end();
-                break;
-        };
-    } catch (err) {
-        console.log(err);
-        initialAction();
-    };
-}
-
-// Selection to view all of the employees.
-const employeeView = async () => {
+// view all employees in the database
+function viewEmployees() {
     
-}
+};
 
-// Selection to view all of the departments.
-const departmentView = async () => {
-    
-}
 
-// Selection to view all of the roles.
-const roleView = async () => {
-   
-}
 
-// Selection to add a new employee.
-const employeeAdd = async () => {
-   
-}
-
-// Selection to add a new department.
-const departmentAdd = async () => {
-   
-}
-
-// Selection to add a new role.
-const roleAdd = async () => {
-   
-}
-
-// Selection to update a roll for a specific employee.
-const employeeUpdate = async () => {
-    
-}
+// exit the app
+function exitApp() {
+    connection.end();
+};
